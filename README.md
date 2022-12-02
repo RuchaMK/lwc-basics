b045d8705126e56e05f09ced85662e1711a775c6
RuchaMK <ruchasf@gmail.com>
2022-12-02 13:17:23 +0530

    using track

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Reference:
 
 https://www.youtube.com/watch?v=Y1vFHoXXilw&list=PLSWzWO4OqYAoXiLpNOYcctXdoAwcxJynH&index=2
+
+#LWC
--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,25 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from "lwc";
+/*LP: name, age only present in a class are called local.
+name //undefined
+DB:  1 way Synchronization b/w Controller and template(HTML)
+js to HTML by default
+HTML to js using methods */
+export default class HelloWorld extends LightningElement {
+  name = "John";
+  @track emp = {
+    empNo: 101,
+    active: "yes",
+    empFirstName: "",
+    empLastName: "",
+    details: ["101 ", this.empLastName]
+  };
+  handleName(event) {
+    this.name = event.target.value;
+    this.emp.empFirstName = event.target.value;
+  }
 
-export default class HelloWorld extends LightningElement {}
+  handleLastName(event) {
+    this.emp.empLastName = event.target.value;
+    this.emp.details[1] = event.target.value;
+  }
+}
