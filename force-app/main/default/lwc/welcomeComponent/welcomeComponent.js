003f85c09fa0407e5ed2d09232d9d9db98665a9e
RuchaMK <ruchasf@gmail.com>
2023-01-14 01:05:05 +0530

    query sleectors

--- force-app/main/default/lwc/welcomeComponent/welcomeComponent.js
+++ force-app/main/default/lwc/welcomeComponent/welcomeComponent.js
@@ -5,7 +5,8 @@ export default class WelcomeComponent extends LightningElement {
     dispParentMsg = "Calculating.."
     myname = "Welcome Component"
     total = 0
-    
+    disp = ''
+    arr = ['arr1','arr2','arr3']
     /*renderedCallback(){
         this.template.querySelector('c-input-component').changeMsg("Calculating..");
     }
@@ -14,8 +15,17 @@ export default class WelcomeComponent extends LightningElement {
     performAdd(event){
         let n1 = event.detail.num1
         let n2 = event.detail.num2
-        this.total = parseInt(n1) + parseInt(n2)
+        this.total = parseInt("111",n1) + parseInt("111",n2)
         this.template.querySelector('c-input-component').changeMsg("Calculation Done");
+    }
+
+    fetchDetails(){
+        let elm = this.template.querySelector('h6')
+        this.disp = elm.innerText
+        console.log(elm.innerText)
+      
+        console.log(this.arr)
+
     }
     
 
